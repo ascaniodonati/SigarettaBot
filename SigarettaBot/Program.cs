ecc93d5a668ab45266321acc59d8d47ae48ad4b5
Ascanio Donati <donatiascanio@gmail.com>
2023-10-16 11:08:04 +0200

    Aggiungere i file di progetto.

---
+++ SigarettaBot/Program.cs
@@ -0,0 +1,45 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace SigarettaBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TelegramBotClient botClient = new TelegramBotClient(Settings.API_TOKEN);
+            ReceiverOptions receiverOptions = new ReceiverOptions() { AllowedUpdates = { } };
+            using CancellationTokenSource cts = new CancellationTokenSource();
+
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            Console.WriteLine("Ciao, bot!");
+            Console.ReadLine();
+        }
+
+        private static Task HandlePollingErrorAsync(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        static async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            //Se non è un messaggio ignoriamo, per ora
+            if (update.Message == null) { return; }
+
+            if (update.Message.Chat.Type != ChatType.Group)
+            {
+                await client.SendTextMessageAsync(update.Message.Chat.Id, "Ciao!\nAggiungimi a un gruppo per iniziare a giocare 😊");
+            }
+        }
+
+
+    }
+}
---
+++ SigarettaBot/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigarettaBot
+{
+    internal static class Settings
+    {
+        public static string API_TOKEN = "2075040018:AAHEncRh5o66CxeWE6o0X_ULPtaVpcLQxfo";
+    }
+}
